f216dc54e59c34f1069b1c7c7241cfe8d0a52c6d
Mateus Andriola <mateusandriola18@gmail.com>
2023-04-21 14:31:57 -0300

    fix(next-auth configs)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { api } from "@/utils/api";
+
+import "@/styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => (
+  <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>
+);
+
+export default api.withTRPC(MyApp);
